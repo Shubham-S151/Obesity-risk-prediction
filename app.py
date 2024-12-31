8b2704d5244d28a8e93258fa68f652c2236f8488
Shubham <shubhamsharma15104@gmail.com>
2024-12-31 08:44:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ import pickle
 with open('model.pkl','rb') as file:
     model = pickle.load(file)
 
-with open('encoder.pkl','rb') as file:
-    encoder = pickle.load(file)
-
 def prediction(input_data):
     pred = model.predict(input_data)
     '''{
